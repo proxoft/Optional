eef3102026f89e6e05228b421ef90be075614501
Michal Strbka <Michal.Strbka@zuehlke.com>
2023-11-19 09:18:46 +0100

    Option<T>

---
+++ Source/Optional/Optional.Tests/Options/ReduceTest.cs
@@ -0,0 +1,37 @@
+﻿namespace Proxoft.Optional.Tests.Options;
+
+public class ReduceTest
+{
+    [Fact]
+    public void ReduceWhenSome()
+    {
+        Option<int> option = 10;
+
+        int result = option.Reduce(20);
+        result
+            .Should()
+            .Be(10);
+    }
+
+    [Fact]
+    public void ReduceWhenNoneGivenValue()
+    {
+        Option<int> option = None.Instance;
+
+        int result = option.Reduce(20);
+        result
+            .Should()
+            .Be(20);
+    }
+
+    [Fact]
+    public void ReduceWhenNoneGivenFunc()
+    {
+        Option<int> option = None.Instance;
+
+        int result = option.Reduce(() => 20);
+        result
+            .Should()
+            .Be(20);
+    }
+}
