c6461a9707b9c2b91b141baa05a0a21efcdf0c70
Michal Strbka <Michal.Strbka@zuehlke.com>
2023-11-19 18:12:05 +0100

    Async Reduce

---
+++ Source/Optional/Optional/OptionOperatorsAsync.cs
@@ -0,0 +1,32 @@
+﻿namespace Proxoft.Optional;
+
+public static class OptionOperatorsAsync
+{
+    public static Task<T> Reduce<T>(
+        this Task<Option<T>> option,
+        Func<T> ifNone)
+    {
+        return option.Reduce(() => Task.FromResult(ifNone()));
+    }
+
+    public static Task<T> Reduce<T>(
+        this Option<T> option,
+        Func<Task<T>> ifNone)
+    {
+        return Task.FromResult(option).Reduce(ifNone);
+    }
+
+    public static Task<T> Reduce<T>(
+        this Task<Option<T>> option,
+        Func<Task<T>> ifNone)
+    {
+        return option
+            .ContinueWith(t =>
+            {
+                return t.Result is Some<T> s
+                    ? Task.FromResult<T>(s)
+                    : ifNone();
+            })
+            .Unwrap();
+    }
+}
