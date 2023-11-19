a6eaa4d6bc26018e375789cc770765b99412c7ce
Michal Strbka <Michal.Strbka@zuehlke.com>
2023-11-19 18:24:12 +0100

    Map async

---
+++ Source/Optional/Optional.Tests/Options/MapAsyncTest.cs
@@ -0,0 +1,94 @@
+﻿namespace Proxoft.Optional.Tests.Options;
+
+public class MapAsyncTest
+{
+    [Fact]
+    public async Task GivenSome_MapAsync1()
+    {
+        Option<int> option = 10;
+
+        Option<string> mapped = await Task.FromResult(option)
+            .Map(x => Task.FromResult(x.ToString()));
+
+        mapped
+            .Should()
+            .BeOfType<Some<string>>();
+
+        string v = (Some<string>)mapped;
+        v.Should()
+         .Be("10");
+    }
+
+    [Fact]
+    public async Task GivenSome_MapAsync2()
+    {
+        Option<int> option = 10;
+
+        Option<string> mapped = await Task.FromResult(option)
+            .Map(x => x.ToString());
+
+        mapped
+            .Should()
+            .BeOfType<Some<string>>();
+
+        string v = (Some<string>)mapped;
+        v.Should()
+         .Be("10");
+    }
+
+    [Fact]
+    public async Task GivenSome_MapAsync3()
+    {
+        Option<int> option = 10;
+
+        Option<string> mapped = await option
+            .Map(x => Task.FromResult(x.ToString()));
+
+        mapped
+            .Should()
+            .BeOfType<Some<string>>();
+
+        string v = (Some<string>)mapped;
+        v.Should()
+         .Be("10");
+    }
+
+    [Fact]
+    public async Task MapWhenNoneAsync1()
+    {
+        Option<int> option = None.Instance;
+
+        Option<string> mapped = await Task.FromResult(option)
+            .Map(x => Task.FromResult(x.ToString()));
+
+        mapped
+            .Should()
+            .BeOfType<None<string>>();
+    }
+
+    [Fact]
+    public async Task MapWhenNoneAsync2()
+    {
+        Option<int> option = None.Instance;
+
+        Option<string> mapped = await option
+            .Map(x => Task.FromResult(x.ToString()));
+
+        mapped
+            .Should()
+            .BeOfType<None<string>>();
+    }
+
+    [Fact]
+    public async Task MapWhenNoneAsync3()
+    {
+        Option<int> option = None.Instance;
+
+        Option<string> mapped = await Task.FromResult(option)
+            .Map(x => x.ToString());
+
+        mapped
+            .Should()
+            .BeOfType<None<string>>();
+    }
+}
--- Source/Optional/Optional/OptionOperatorsAsync.cs
+++ Source/Optional/Optional/OptionOperatorsAsync.cs
@@ -2,6 +2,37 @@
 
 public static class OptionOperatorsAsync
 {
+    public static Task<Option<NT>> Map<T, NT>(
+        this Task<Option<T>> option,
+        Func<T, NT> map)
+    {
+        return option
+            .ContinueWith(
+                t => t.Result.Map(map)
+            );
+    }
+
+    public static Task<Option<NT>> Map<T, NT>(
+        this Option<T> option,
+        Func<T, Task<NT>> map)
+    {
+        return Task.FromResult(option)
+            .Map(map);
+    }
+
+    public static Task<Option<NT>> Map<T, NT>(
+        this Task<Option<T>> option,
+        Func<T, Task<NT>> map)
+    {
+        return option
+            .ContinueWith(
+                t => t.Result is Some<T> s
+                    ? map(s).ContinueWith<Option<NT>>(m => m.Result)
+                    : Task.FromResult<Option<NT>>(None.Instance)
+            )
+            .Unwrap();
+    }
+
     public static Task<T> Reduce<T>(
         this Task<Option<T>> option,
         Func<T> ifNone)
