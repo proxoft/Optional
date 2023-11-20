83a756dcb9b8c9b949d617d46448ae401553925b
Michal Strbka <Michal.Strbka@zuehlke.com>
2023-11-20 16:27:20 +0100

    Option adapter operators

---
+++ Source/Optional/Optional/OptionAdapters.cs
@@ -0,0 +1,30 @@
+﻿namespace Proxoft.Optional;
+
+public static class OptionAdapters
+{
+    public static Either<L, R> Either<L, R>(
+        this Option<R> option,
+        L ifNone)
+    {
+        return option.Either(() => new Left<L, R>(ifNone));
+    }
+
+    public static Either<L, R> Either<L, R>(
+        this Option<R> option,
+        Func<Either<L, R>> ifNone)
+    {
+        return option
+            .Map(r => (Either<L, R>)r)
+            .Reduce(ifNone);
+    }
+
+    public static Either<L, R> Either<T, L, R>(
+        this Option<T> option,
+        Func<T, Either<L, R>> some,
+        Func<Either<L,R>> none)
+    {
+        return option
+            .Map(some)
+            .Reduce(none);
+    }
+}
---
+++ Source/Optional/Optional/OptionAdaptersAsync.cs
@@ -0,0 +1,51 @@
+﻿namespace Proxoft.Optional;
+
+public static class OptionAdaptersAsync
+{
+    public static Task<Either<L, R>> Either<L, R>(
+        this Task<Option<R>> option,
+        L ifNone)
+    {
+        return option
+            .ContinueWith(
+                o => o.Result.Either(ifNone)
+            );
+    }
+
+    public static Task<Either<L, R>> Either<L, R>(
+        this Option<R> option,
+        Func<Task<Either<L, R>>> ifNone)
+    {
+        return Task.FromResult(option)
+            .Either(ifNone);
+    }
+
+    public static Task<Either<L, R>> Either<L, R>(
+        this Task<Option<R>> option,
+        Func<Task<Either<L, R>>> ifNone)
+    {
+        return option
+            .ContinueWith(ot =>
+            {
+                return ot.Result
+                    .Map(v => Task.FromResult<Either<L, R>>(v))
+                    .Reduce(ifNone);
+            })
+            .Unwrap();
+    }
+
+    public static Task<Either<L, R>> Either<T, L, R>(
+        this Task<Option<T>> option,
+        Func<T, Task<Either<L, R>>> some,
+        Func<Task<Either<L, R>>> none)
+    {
+        return option
+            .ContinueWith(o =>
+            {
+                return o.Result
+                    .Map(some)
+                    .Reduce(none);
+            })
+            .Unwrap();
+    }
+}
