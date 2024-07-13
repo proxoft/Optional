efd5f29cc1fd9860ab047209cbd96362627ea209
Michal Strbka <Michal.Strbka@zuehlke.com>
2024-07-13 22:37:12 +0200

    JsonConverter for Option{T}

---
+++ Source/Optional/Optional.Tests/Serialization/OptionSerializationTest.cs
@@ -0,0 +1,79 @@
+﻿using System.Text.Json;
+using Proxoft.Optional.Serialization;
+
+namespace Proxoft.Optional.Tests.Serialization;
+
+public class OptionSerializationTest
+{
+    JsonSerializerOptions _options;
+
+    public OptionSerializationTest()
+    {
+        _options = new JsonSerializerOptions();
+        _options.Converters.Add(new OptionJsonConverter<int>());
+        _options.Converters.Add(new OptionJsonConverter<Foo>());
+        _options.Converters.Add(new OptionJsonConverter<int[]>());
+    }
+
+    [Fact]
+    public void SerializeSomeInt()
+    {
+        Option<int> someInt = new Some<int>(42);
+
+        string json = JsonSerializer.Serialize(someInt, _options);
+        json
+            .Should()
+            .Be("{\"$optional\":\"some\",\"value\":42}");
+    }
+
+    [Fact]
+    public void SerializeNoneInt()
+    {
+        Option<int> noneInt = None.Instance;
+
+        string json = JsonSerializer.Serialize(noneInt, _options);
+        json
+            .Should()
+            .Be("{\"$optional\":\"none\"}");
+    }
+
+    [Fact]
+    public void SerializeNoneInt2()
+    {
+        Option<int> noneInt = None<int>.Instance;
+
+        string json = JsonSerializer.Serialize(noneInt, _options);
+        json
+            .Should()
+            .Be("{\"$optional\":\"none\"}");
+    }
+
+    [Fact]
+    public void SerializeSomeFoo()
+    {
+        Option<Foo> someFoo = new Foo()
+        {
+            FooName = "Parent",
+            Child = new Foo
+            {
+                FooName = "Child"
+            }
+        };
+
+        string json = JsonSerializer.Serialize(someFoo, _options);
+        json
+            .Should()
+            .Be("{\"$optional\":\"some\",\"value\":{\"FooName\":\"Parent\",\"Child\":{\"FooName\":\"Child\",\"Child\":null}}}");
+    }
+
+    [Fact]
+    public void SerializeArrayOfInt()
+    {
+        Option<int[]> some = new int[] { 1, 6, 9 };
+        string json = JsonSerializer.Serialize(some, _options);
+
+        json
+            .Should()
+            .Be("{\"$optional\":\"some\",\"value\":[1,6,9]}");
+    }
+}
---
+++ Source/Optional/Optional/Serialization/OptionJsonConverter.cs
@@ -0,0 +1,103 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Proxoft.Optional.Serialization;
+
+public class OptionJsonConverter<T> : JsonConverter<Option<T>>
+{
+    public override bool CanConvert(Type typeToConvert)
+    {
+        bool canConvert = typeToConvert == typeof(Option<T>)
+            || typeToConvert == typeof(Some<T>)
+            || typeToConvert == typeof(None<T>);
+
+        return canConvert;
+    }
+
+    public override Option<T> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        string optionalType = reader.ReadOptionalType();
+
+        Option<T> result;
+        switch (optionalType)
+        {
+            case "some":
+                reader.ReadValuePropertyToken();
+                T? value = JsonSerializer.Deserialize<T>(ref reader, options);
+                if(value is null)
+                {
+                    throw new Exception("value cannot be null");
+                }
+                result = value;
+                break;
+            case "none":
+                result = None<T>.Instance;
+                break;
+            default:
+                throw new ArgumentException("allowed values are 'none' or 'some'");
+        }
+
+        reader.ReadEndToken();
+        return result;
+    }
+
+    public override void Write(Utf8JsonWriter writer, Option<T> value, JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+        if (value is Some<T> some)
+        {
+            writer.WriteString("$optional", "some");
+            writer.WritePropertyName("value");
+            T v = (T)some;
+            JsonSerializer.Serialize(writer, v, options);
+        }
+        else
+        {
+            writer.WriteString("$optional", "none");
+        }
+        writer.WriteEndObject();
+    }
+}
+
+file static class ReaderExtensions
+{
+    public static string ReadOptionalType(this ref Utf8JsonReader reader)
+    {
+        if(!reader.Read() || reader.TokenType != JsonTokenType.PropertyName)
+        {
+            throw new InvalidOperationException("Error reading token for property '$optional'");
+        }
+
+        if (!reader.Read() || reader.TokenType != JsonTokenType.String)
+        {
+            throw new InvalidOperationException("Error reading token for property '$optional': expected string");
+        }
+
+        string optionalType = reader.GetString() ?? "";
+        return optionalType;
+    }
+
+    public static void ReadValuePropertyToken(this ref Utf8JsonReader reader)
+    {
+        if (!reader.Read() || reader.TokenType != JsonTokenType.PropertyName)
+        {
+            throw new InvalidOperationException("Error reading token for property '$optional'");
+        }
+
+        string propertyName = reader.GetString() ?? "";
+        if (propertyName != "value")
+        {
+            throw new InvalidOperationException($"{propertyName} does not exist");
+        }
+
+        reader.Read();
+    }
+
+    public static void ReadEndToken(this ref Utf8JsonReader reader)
+    {
+        if(!reader.Read() || reader.TokenType != JsonTokenType.EndObject)
+        {
+            throw new InvalidOperationException("Expected EndObject token.");
+        }
+    }
+}
